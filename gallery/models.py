0f913aa14f8393db02fa1c606d9b7522cb77d296
Koech-code <nixon.koech@moringaschool.com>
2021-09-04 02:49:19 +0300

    added to my models

--- gallery/models.py
+++ gallery/models.py
@@ -5,16 +5,32 @@ from django.db.models.deletion import CASCADE
 class Category(models.Model):
     Categories=models.TextField(max_length=25)
     
+    def save_category(self):
+        self.save()
+
+    def __str__(self):
+        return self.Categories
 
 class Location(models.Model):
     place= models.CharField(max_length=30)
 
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.place
 
 class Image(models.Model):
     image= models.ImageField(upload_to='images/')
-    name= models.CharField(max_length=20)
+    img_name= models.CharField(max_length=20)
     description=models.TextField(max_length=200)
     image_category=models.ForeignKey(Category, on_delete=CASCADE)
     image_location=models.ForeignKey(Location, on_delete=CASCADE)
 
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
     
