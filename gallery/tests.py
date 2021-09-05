6320e1cdd403fa0292e47e51cec7e8a7d2638358
Koech-code <nixon.koech@moringaschool.com>
2021-09-05 20:54:33 +0300

    created test for the setup, test instance, saving and deleting images, categories and locations

--- gallery/tests.py
+++ gallery/tests.py
@@ -27,4 +27,45 @@ class ImageTestClass(TestCase):
         deleted=Image.objects.all()
         self.assertTrue(len(deleted) == 0) 
 
-    
+    def test_search_by_category(self):
+        image_category='food'
+        result=self.image.search_by_category(image_category)
+        self.assertTrue(len(result) >=0)
+
+class TestCategoryClass(TestCase):
+    def setUp(self):
+        self.image_category=Category(Categories='food')
+        self.image_category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_category, Category))
+
+    def test_save_category(self):
+        self.image_category.save_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    def test_delete_category(self):
+        self.image_category.delete_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category) ==0)
+
+class TestLocationClass(TestCase):
+    def setUp(self):
+        self.image_location=Location(place='Gelegele')
+        self.image_location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_location, Location))
+
+    def test_save_location(self):
+        self.image_location.save_location()
+        location=Location.objects.all()
+        self.assertTrue(len(location) > 0)
+
+    def test_delete_location(self):
+        self.image_location.delete_location()
+        location=Location.objects.all()
+        self.assertTrue(len(location) ==0)
+
+
